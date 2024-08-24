abb5d8ac4bac42977634685dda75808a05d1148a
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-08-23 22:02:12 -0400

    Update optimize-ag-dot-angle-multiobj_python.py

--- optimize-ag-dot-angle-multiobj_python.py
+++ optimize-ag-dot-angle-multiobj_python.py
@@ -210,9 +210,12 @@ def sim(run_file, filenames=[], input_lines=[]):
                     ]
                     +
                     #[f"mpirun -np 32 python -m mpi4py {new_file} True | tee -a {air_raw_path} ; grep flux1: {air_raw_path} > {air_data_path};\n",
-                    [f"mpirun -np 32 python -m mpi4py {new_file} True | tee -a {air_raw_path} ; grep flux1: {air_raw_path} > {air_data_path};\n",
-
-                     f"mpirun -np 32 python -m mpi4py {new_file} False | tee -a {metal_raw_path} ; grep flux1: {metal_raw_path} > {metal_data_path};\n"
+                    [
+                        #f"mpirun -np 32 python -m mpi4py {new_file} True | tee -a {air_raw_path} ; grep flux1: {air_raw_path} > {air_data_path};\n",
+                        #f"mpirun -np 32 python -m mpi4py {new_file} False | tee -a {metal_raw_path} ; grep flux1: {metal_raw_path} > {metal_data_path};\n",
+                        f"python -u {new_file} True | tee -a {air_raw_path} ; grep flux1: {air_raw_path} > {air_data_path};\n",
+                        f"python -u {new_file} False | tee -a {metal_raw_path} ; grep flux1: {metal_raw_path} > {metal_data_path};\n"
+                     
                      ]
                     + 
                     [
