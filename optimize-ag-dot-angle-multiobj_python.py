4d556d40e5960cc2d38dcc7d8f5cb091bcacee8c
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-08-24 20:56:52 -0400

    Update optimize-ag-dot-angle-multiobj_python.py

--- optimize-ag-dot-angle-multiobj_python.py
+++ optimize-ag-dot-angle-multiobj_python.py
@@ -212,7 +212,7 @@ def sim(run_file, filenames=[], input_lines=[]):
                     #[f"mpirun -np 32 python -m mpi4py {new_file} True | tee -a {air_raw_path} ; grep flux1: {air_raw_path} > {air_data_path};\n",
                     [
                         #f"mpirun -np 32 python -m mpi4py {new_file} True | tee -a {air_raw_path} ; grep flux1: {air_raw_path} > {air_data_path};\n",
-                        f"python -m mpi4py {new_file} True | tee -a {air_raw_path} ; grep flux1: {air_raw_path} > {air_data_path};\n",
+                        f"mpirun -np 32 python -m mpi4py {new_file} True | tee -a {air_raw_path} ; grep flux1: {air_raw_path} > {air_data_path};\n",
 
                         f"mpirun -np 32 python -m mpi4py {new_file} False | tee -a {metal_raw_path} ; grep flux1: {metal_raw_path} > {metal_data_path};\n",
                         #f"python {new_file} True | tee -a {air_raw_path} ; grep flux1: {air_raw_path} > {air_data_path};\n",
