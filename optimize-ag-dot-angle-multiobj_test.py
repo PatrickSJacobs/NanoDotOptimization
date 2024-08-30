fcf9d6a4191cfc8f174a15432aabbf5f78f9be83
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-08-30 15:37:30 -0400

    Update optimize-ag-dot-angle-multiobj_test.py

--- optimize-ag-dot-angle-multiobj_test.py
+++ optimize-ag-dot-angle-multiobj_test.py
@@ -432,7 +432,7 @@ if __name__ == "__main__":
         writer.writerow(["filename", "sr", "ht", "cs", "theta_deg", "b-param", "c-param", "b_var", "c_var","execution time", "step count"])
         file.close()
 
-    max_evaluations = 2
+    max_evaluations = 70
 
     '''
 
@@ -452,7 +452,7 @@ if __name__ == "__main__":
     algorithm = GDE3(
         population_evaluator=MultiprocessEvaluator(processes=16),
         problem=problem,
-        population_size=1,
+        population_size=16,
         cr=0.9,
         f=0.8,
         termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations),
