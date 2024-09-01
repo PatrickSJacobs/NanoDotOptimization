4ad7abb01ec62f1b3104ea979cc3d3c6fc4716a0
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-09-01 16:03:29 -0400

    test

--- optimize-ag-dot-angle-multiobj.py
+++ optimize-ag-dot-angle-multiobj.py
@@ -440,8 +440,8 @@ if __name__ == "__main__":
         writer.writerow(["filename", "sr", "ht", "cs", "theta_deg", "b-param", "c-param", "b_var", "c_var","execution time", "step count"])
         file.close()
 
-    #max_evaluations = 640
-    max_evaluations = 5
+    max_evaluations = 640
+    #max_evaluations = 5
 
     '''
 
@@ -462,7 +462,7 @@ if __name__ == "__main__":
         population_evaluator=MultiprocessEvaluator(processes=16),
         problem=problem,
         #population_size=16,
-        population_size=5,
+        population_size=16,
         cr=0.9,
         f=0.8,
         termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations),
@@ -471,9 +471,7 @@ if __name__ == "__main__":
 
 
     algorithm.run()
-    solutions = algorithm.get_result()
     front = algorithm.get_result()
-    #front = get_non_dominated_solutions(solutions)
 
     for sol in range(len(front)):
         vars = front[sol].variables
