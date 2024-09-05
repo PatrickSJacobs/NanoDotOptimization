c725899a57e115e970eab220fa21598c1b21535d
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-09-05 17:52:09 -0400

    test

--- optimize-ag-dot-angle-multiobj_test.py
+++ optimize-ag-dot-angle-multiobj_test.py
@@ -26,7 +26,6 @@ from scipy.signal import find_peaks
 import sys
 import traceback
 
-
 current_time = datetime.now().strftime("%m_%d_%Y__%H_%M_%S")# Getting the current time
 main_home_dir = "/home1/08809/tg881088/" # Home directory for optimization
 folder_name = "opt_%s" % str(current_time)# Folder name for optimization files
@@ -67,97 +66,6 @@ def make_filename(sr, ht, cs, theta_deg):
                                                       display_theta_deg,
                                                       )  # filename to be used
     return filename
-
-def obj_func_calc(wvls, R_meep):
-    '''
-    (6) Objective Function Execution
-        - Fit distribution of reflectance to a multimodal distribution
-        - Get means, variances and heights of n subdistributions
-        - Calculate vector peak_eval = [norm(mean(1), variance(1), 1/height(1)), norm(mean(2), variance(2), 1/height(2)), ..., norm(mean(n), variance(n), 1/height(n))]
-        - Return sum of all elements in peak_eval
-    '''
-
-    # Make histogram based on reflectance data
-    K = 2
-
-    xs = wvls
-    ys = R_meep
-
-    xs = xs[: len(xs) - K]
-    ys = ys[: len(ys) - K]
-
-    mam = max(ys)
-    ind = []
-    maxis = []
-    ty = len(ys)
-    for i in range(ty):
-        j = ys[i]
-        if j >= 0.5 * mam:
-            maxis += [j]
-            ind += [i]
-
-    ind = [i for i in range(min(ind), max(ind) + 1)]
-    maxis = [ys[i] for i in ind]
-
-    neg_ys_prime = [element + mam for element in [(-1) * y for y in maxis]]
-    neg_ys = [element + mam for element in [(-1) * y for y in ys]]
-
-    peaks, _ = find_peaks(neg_ys_prime)
-    maxi = 0
-
-    if len(peaks) > 0:
-        neg_min = mam
-        real_min = 0
-
-        for peak in peaks:
-            if neg_min >= neg_ys[ind[peak]]:
-                neg_min = neg_ys[ind[peak]]
-                real_min = ys[ind[peak]]
-
-        index_list = [i for i in range(len(ys)) if ys[i] >= real_min]
-
-        ys_fixed = [ys[i] for i in index_list]
-
-        # Use a cumulative sum instead of creating a large list
-        weighted_sum = sum(wvl * freq for wvl, freq in zip([xs[i] for i in index_list], np.ceil(np.array(ys_fixed) / np.min(ys_fixed))))
-        total_freq = sum(np.ceil(np.array(ys_fixed) / np.min(ys_fixed)))
-        
-        if total_freq > 0:
-            maxi = weighted_sum / total_freq
-        else:
-            maxi = xs[ys.index(mam)]
-
-    else:
-        maxi = xs[ys.index(mam)]
-
-    # tsr = sum(x * i for i, x in enumerate(L, 1)) / len(L)
-
-    # print(tsr)
-
-    q = 1000
-
-    # 1/(1 + (q/b*(x - a))^2)/c == 1/(pc)
-
-    def objective(x, b, c):
-        # maxi = np.array([xs[ys.index(mam)] for i in range(len(x))])
-        # maxi = sum(u * i for i, u in enumerate(x, 1)) / len(x)
-        maximum = np.array([maxi for i in x])
-
-        #f = q / (1 + (q / b * (x - maximum)) ** 2) / c
-        f = 1 / (c**2*(1 + (q / b * (x - maximum)) ** 2))
-
-        return f
-
-    popt, popv = curve_fit(objective, xs, ys)
-
-    b, c = popt
-    b_var = popv[0][0]
-    c_var = popv[1][1]
-
-    printing("finished obj_eval")
-
-    return b, c**2 * 10 - 10, b_var * 100, c_var * 100
-
 
 def obj_func_run(x: [float]):
     """
@@ -211,6 +119,21 @@ def obj_func_run(x: [float]):
     # cell_size = 2*(sr + cs)
     cell_size = 2 * sr + cs
 
+    info = [progress_file, 
+            air_data_path, 
+            metal_data_path, 
+            file_home_path, 
+            file_work_path, 
+            filename, 
+            ticker_file, 
+            air_raw_path, 
+            metal_raw_path, 
+            sr, 
+            ht, 
+            cs, 
+            theta_deg]
+    
+
     file1.writelines(["#!/bin/bash%s" % "\n",
                       "#SBATCH -J myMPI%s" % "\n",
                       "#SBATCH -o myMPI.%s%s" % ("o%j", "\n"),
@@ -238,6 +161,7 @@ def obj_func_run(x: [float]):
                       "mpirun -np 32 meep no-metal?=false sr=%s ht=%s sy=%s theta_deg=%s %s |tee %s;%s" % (sr, ht, cell_size, theta_deg, new_file, metal_raw_path, "\n"),
                       #"meep sr=%s ht=%s sy=%s theta_deg=%s %s |tee %s;%s" % (sr, ht, cell_size, theta_deg, new_file, metal_raw_path, "\n"),
                       "grep flux1: %s > %s;%s" % (metal_raw_path, metal_data_path, "\n"),
+                      "python %s %s;%s" % (main_home_dir + "NanoDotOptimization/optimize-ag-dot-angle-evaluate.py", info, "\n"),
                       "rm -r %s %s" % (ticker_file, "\n"),
                       "echo 1 >> %s %s" % (ticker_file, "\n")
 
@@ -269,91 +193,22 @@ def obj_func_run(x: [float]):
 
         time_count = time_count + 1
         time.sleep(1)
-
-    b, c, b_var, c_var = 1E6, 1E6, 1E6, 1E6
-    # Check if data is good and data file exists, if not error
-    if os.path.isfile(metal_data_path) and os.path.isfile(air_data_path):
-        df = None
-        df0 = None
-        try:
-            df = pd.read_csv(metal_data_path, header=None)
-            df0 = pd.read_csv(air_data_path, header=None)
         
-            printing("success df")
-
-            # Get wavelengths and reflectance data
-            wvls = []
-            R_meep = []
-            
-            for r, r0, wvl in zip(df[3], df0[3], df[1]):
-                try:
-                    ration = np.abs(- float(r) / float(r0))
-                    R_meep += [ration]
-                    wvls += [float(wvl) * 0.32]
-                    
-                except:
-                    pass
-
-            wvls = wvls[: len(wvls) - 2]
-            R_meep = R_meep[: len(R_meep) - 2]
-
-            #print(wvls)
-            #print(R_meep)
-            
-            log_obj_exe = open(file_home_path + "calc_log_obj.csv", 'r').readlines()
-            step_count = int(len(log_obj_exe))
-
-            with open(file_work_path + "calc_log_data_step_%s_%s.csv" % (step_count, filename), 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(["wvl", "refl"])
-                for (wvl, refl) in zip(wvls, R_meep):
-                    writer.writerow([wvl, refl])
-            printing("passed to obj")
-            # (5) Sending Data Through Objective function
-
-            b, c, b_var, c_var = obj_func_calc(wvls, R_meep)
-
-            printing("came out of obj")
-
-            #execution_dictionary[filename] = {"b": b, "c": c, "b_var": b_var, "c_var": c_var}
-            
-        except Exception as e:
-            print("An error occurred:")
-            print(f"air_data_path: {air_data_path}")
-            print(f"metal_data_path: {metal_data_path}")
-            print(f"ticker_file: {ticker_file}")
-            print(f"air_raw_path: {air_raw_path}")
-            print(f"metal_raw_path: {metal_raw_path}")
-            print(f"new_file: {new_file}")
-            traceback.print_exc()
-            sys.exit()
-
-    else:
-        printing(f"({metal_data_path}): It isn't a file! Error in variables {x} ")
-        #raise ValueError(f"({metal_data_path}): It isn't a file!")
-   
-    # (7) Logging of Current Data
-    with open(file_home_path + "calc_log_obj.csv", 'a') as file:
-        writer = csv.writer(file)
-        writer.writerow([filename, sr, ht, cs, theta_deg, b, c, b_var, c_var, datetime.now().strftime("%m_%d_%Y__%H_%M_%S"), step_count])
-        file.close()
-        
-    # (8) Deleting Excess Files
-    sleep(10)
     os.system("ssh login1 rm -r " +
-              ticker_file + " " +
-              air_raw_path + " " +
-              metal_raw_path + " " +
-              metal_data_path + " " +
-              air_data_path + " " +
-              new_file + " " +
-              main_home_dir + "ag-dot-angle" + code + "* " +
-              file_home_path + "ag-dot-angle" + code + "* ")
+            ticker_file + " " +
+            air_raw_path + " " +
+            metal_raw_path + " " +
+            metal_data_path + " " +
+            air_data_path + " " +
+            new_file + " " +
+            main_home_dir + "ag-dot-angle" + code + "* " +
+            file_home_path + "ag-dot-angle" + code + "* ")
 
     printing(f"finished deleting files; code: {code}")
 
     # (9) Returning of Result and Continuity of Optimization
     printing(f'Executed: {filename}')
+    
     return filename
 
 def get_values(x: [float], param: str):
@@ -420,9 +275,10 @@ problem = (
     .set_name("Testing")
     .add_variable(0.001 * 5, 0.001 * 125)
     .add_variable(0.001 * 50, 0.001 * 100)
-    .add_variable(0.001 * 25, 0.001 * 250)
-    .add_variable(0.0, 0.0)
-    #.add_variable(0.0, 90.0)
+    #.add_variable(0.001 * 25, 0.001 * 250)
+    .add_variable(0.001 * 250, 0.001 * 250)
+    #.add_variable(0.0, 0.0)
+    .add_variable(0.0, 90.0)
     .add_function(c)
     .add_function(b)
     .add_function(b_var)
@@ -440,8 +296,8 @@ if __name__ == "__main__":
         writer.writerow(["filename", "sr", "ht", "cs", "theta_deg", "b-param", "c-param", "b_var", "c_var","execution time", "step count"])
         file.close()
 
-    max_evaluations = 64
-    #max_evaluations = 5
+    #max_evaluations = 64
+    max_evaluations = 1
 
     '''
 
@@ -461,8 +317,8 @@ if __name__ == "__main__":
     algorithm = GDE3(
         population_evaluator=MultiprocessEvaluator(processes=16),
         problem=problem,
-        population_size=16,
-        #population_size=5,
+        #population_size=16,
+        population_size=1,
         cr=0.5,
         f=0.8,
         termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations),
