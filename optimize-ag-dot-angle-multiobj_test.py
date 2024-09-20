3e3020a76bc7f4db875419eef6637eb047750744
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-09-19 23:59:28 -0400

    Update optimize-ag-dot-angle-multiobj_test.py

--- optimize-ag-dot-angle-multiobj_test.py
+++ optimize-ag-dot-angle-multiobj_test.py
@@ -281,7 +281,7 @@ if __name__ == "__main__":
     ## Ensure train_Y has the correct shape
     #train_Y = train_Y.view(-1, 4)
     
-    train_Y = train_Y.view(-1, 1)
+    train_Y = train_Y.reshape(-1, 1)
 
     # Bounds (include 'cs' bounds)
     bounds = torch.tensor([
