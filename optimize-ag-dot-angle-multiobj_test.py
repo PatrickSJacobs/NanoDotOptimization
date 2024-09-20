98acc521b08295a13ab7cf1162e1bae3f976472c
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-09-20 02:03:14 -0400

    Update optimize-ag-dot-angle-multiobj_test.py

--- optimize-ag-dot-angle-multiobj_test.py
+++ optimize-ag-dot-angle-multiobj_test.py
@@ -314,11 +314,7 @@ if __name__ == "__main__":
         
         #fit_fully_bayesian_model_nuts(model)
         fit_fully_bayesian_model_nuts(
-            model,
-            warmup_steps=2,  # Reduce from 512 to 128
-            num_samples=128,   # Reduce from 256 to 128
-            thinning=16,       # Adjust thinning if necessary
-            disable_progbar=False  # You can set this to True to disable the progress bar
+            model
         )
 
         #posterior = mtsaas_gp.posterior(test_X)
