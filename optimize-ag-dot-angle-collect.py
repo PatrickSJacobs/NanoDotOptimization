91c705e10f8cf50192c5b3b9f193389bc11e8b6e
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-09-21 00:50:02 -0400

    Update optimize-ag-dot-angle-collect.py

--- optimize-ag-dot-angle-collect.py
+++ optimize-ag-dot-angle-collect.py
@@ -255,7 +255,7 @@ def main():
 
             print(type(dataset_df))
             # Append the row to the DataFrame
-            dataset_df = dataset_df.concat({
+            dataset_df =  pd.concat([dataset_df, {
                 "path": path,
                 "sr": sr,
                 "ht": ht,
@@ -266,8 +266,7 @@ def main():
                 "b_var": b_var,
                 "c_var": c_var,
                 "count": count
-            }, ignore_index=True)
-
+            }], ignore_index=True)
         except Exception as e:
             print(f"Error processing file {path}: {e}")
             continue
