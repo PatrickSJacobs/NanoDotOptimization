9424a9545389cf7d493f4661e6a4fab0181bf610
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-09-21 02:27:13 -0400

    Update optimize-ag-dot-angle-collect.py

--- optimize-ag-dot-angle-collect.py
+++ optimize-ag-dot-angle-collect.py
@@ -317,7 +317,7 @@ def main():
     dataset_df.to_csv(training_file, index=False)
     print(f"Collected dataset contains {len(dataset_df)} records before pruning.")
 
-    num_points = 400
+    num_points = 450
     # Prune the dataset
     df_final = prune_dataset(
         dataset_df, 
