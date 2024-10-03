335edab4149bde15af0b478952979a39e7fec5a2
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-10-03 13:44:40 -0400

    Update optimize-ag-dot-angle-multiobj_explore.py

--- optimize-ag-dot-angle-multiobj_explore.py
+++ optimize-ag-dot-angle-multiobj_explore.py
@@ -325,8 +325,8 @@ if __name__ == "__main__":
         file.close()
     
     #max_evaluations = 8
-    population_size = 3
-    max_evaluations = 3
+    population_size = 6
+    max_evaluations = 6
     
     '''
     from pymoo.util.nds.non_dominated_sorting import NonDominatedSorting
