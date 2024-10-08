3ebf06074ec2545f84632dd2779ffa49827a0d36
PatrickSJacobs <pat.steph.jacobs@gmail.com>
2024-10-07 20:29:05 -0400

    Update optimize-ag-dot-angle-collect.py

--- optimize-ag-dot-angle-collect.py
+++ optimize-ag-dot-angle-collect.py
@@ -341,7 +341,7 @@ def main():
         num_points, 
         #{ "c-param": 100, "b_var": 11, }
         #{ "c-param": 2, "b-param": 2, "b_var": 2, }
-        { "c-param": 5.5, "b-param": 30, "b_var": 65, "c_var": 0.8, }
+        { "c-param": 5.8, "b-param": 30, "b_var": 65, "c_var": 1, }
 
         )
 
